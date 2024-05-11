5cd81bbef8fcf4a7104a1293749c9fc75852e1f5
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2024-05-11 23:24:41 +0530

    added CORS and changed return type

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI, File, UploadFile
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+]
+
+
 
 app = FastAPI()
 
-@app.post("/predict")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/predict/")
 def predict(file: UploadFile = File(...)):
 
     df = pd.read_csv(file.file)
 
     records = df.to_dict('records')
     
-    return {"data": records}
+    return records
 
